b3afcb24df63a2f981cf416f51b9d9d3d209a50c
eldabc <elda822@gmail.com>
2020-08-19 23:54:56 -0400

    Prueba de Rutas

--- routes/api.php
+++ routes/api.php
@@ -13,6 +13,12 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::get('tasks', 'TaskController@index');
+Route::get('task/{id}', 'TaskController@show');
+Route::post('task', 'TaskController@store');
+Route::put('task/{id}', 'TaskController@update');
+Route::delete('task/{id}', 'TaskController@delete');
+
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
